6a2bbd2f01038e8f240a29b7711244459e31ff8a
sumitd <sumitd@c3ihub.iitk.ac.in>
2025-01-14 14:56:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'cellsegmentation',
+    version = '0.0.0',
+    author= 'Sumit dhar Dwivedi',
+    author_email= 'ddwivedisumit1523@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+) 
